ecd3fd38b4e607f5016370459dcccc48e3b39632
Tushar010402 <tusharagrawal0104@gmail.com>
2024-05-17 00:23:24 +0530

    resolve AWS error 20

--- src/pages/entries.js
+++ src/pages/entries.js
@@ -13,7 +13,10 @@ const EntriesList = () => {
 
   const fetchData = async () => {
     try {
-      const encodedCredentials = btoa(`${process.env.NEXT_PUBLIC_USERNAME}:${process.env.NEXT_PUBLIC_PASSWORD}`);
+      const username = 'TusharAgrawal098';
+      const password = 'TusharAgrawal@1234567890';
+      const credentials = `${username}:${password}`;
+      const encodedCredentials = btoa(credentials);
 
       const response = await fetch(`/api/entries?page=${pageNumber}&action_text=${statusFilter}`, {
         headers: {
@@ -46,7 +49,10 @@ const EntriesList = () => {
 
   const handleSubmit = async (entryId, status, scheduledDateTime, remark) => {
     try {
-      const encodedCredentials = btoa(`${process.env.NEXT_PUBLIC_USERNAME}:${process.env.NEXT_PUBLIC_PASSWORD}`);
+      const username = 'TusharAgrawal098';
+      const password = 'TusharAgrawal@1234567890';
+      const credentials = `${username}:${password}`;
+      const encodedCredentials = btoa(credentials);
 
       const response = await fetch(`/api/entries/${entryId}`, {
         method: 'POST',
@@ -145,6 +151,7 @@ const EntriesList = () => {
             <th>Actions</th>
             <th>Remark</th>
             <th>Update</th>
+
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
